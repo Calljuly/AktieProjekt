63534a54d9e0d866d11e0c4c5700fbe3173fea4b
Julia Call <54210280+Calljuly@users.noreply.github.com>
2020-06-03 11:00:53 +0200

    Styled
    Stylde infor bow med styled components

--- src/Components/home/InfoBox.js
+++ src/Components/home/InfoBox.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Infobox = (props) => {
+
+    const InfoBoxStyle = styled.div`
+    background-color: #FFD98D;
+    font-size: x-large;
+    padding: 20px;
+    padding-left: 20px;
+    width: 95%;
+    float: left; 
+    margin: 40px;
+    margin-top:20px;
+    text-align: left;
+     `;
+
     return (
-        <div className="infoBox">
-        <p>{props.text}</p>
-        </div>)
+        <InfoBoxStyle>
+            <p>{props.text}</p>
+        </InfoBoxStyle>)
 }
 export default Infobox;
