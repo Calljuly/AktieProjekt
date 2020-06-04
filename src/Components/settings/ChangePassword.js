654a83432077a1724ad4de0f2ef29f8adc79d577
Julia Call <54210280+Calljuly@users.noreply.github.com>
2020-06-04 10:07:04 +0200

    Style för changePass
    Tillagde styles för change password sidan

--- src/Components/settings/ChangePassword.js
+++ src/Components/settings/ChangePassword.js
@@ -3,33 +3,49 @@ import styled from 'styled-components'
 
 const ChangePassword = () =>{
     const ChangePassDivStyle = styled.div`
-
+    
+    width: 90%;
+    height: 70%;
     
     `;
+    const PasswordForm = styled.form`
+        display: flexbox;
+        width:70%;
+        text-align:left;
+        padding:30px; 
+        justify-content: left;
+        
+    `;
     const InputStyle = styled.input`
-    
+    width: 100%;
+    height: 39px;
     border: 1px solid #EBEDF2;
     border-radius: 4px;
     opacity: 1;
+    margin:10px;
     `;
     const LableStyle = styled.label`
+    width: 100%;
+    height: 12px;
     text-align: left;
-    font: Medium 12px/17px Poppins;
+    font-family:Poppins;
+    font-size:30px;
     letter-spacing: -0.01px;
     color: #A1A8C3;
-    opacity: 1;    
+    opacity: 1;
+    margin:10px;     
     `;
 
-    return (<div>
-                <form>
+    return (<ChangePassDivStyle>
+                <PasswordForm>
                     <LableStyle name="CurrentPass">Nuvarande lösenord : </LableStyle>
                     <InputStyle type="text" value="currentPass"></InputStyle>
                     <LableStyle name="NewPass">Nytt lösenord : </LableStyle>
                     <InputStyle type="password" value="newPass"></InputStyle>
                     <LableStyle name="NewPassAgain">Bekräfta nytt lösenord : </LableStyle>
                     <InputStyle type="password" value="newPassAgain"></InputStyle>
-                </form>
-        </div>)
+                </PasswordForm>
+        </ChangePassDivStyle>)
 
 }
 export default ChangePassword;
