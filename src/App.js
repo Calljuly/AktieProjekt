81d663b4145c36ecaef06dca05f0e49c3cc16eb5
Julia Call <54210280+Calljuly@users.noreply.github.com>
2020-06-07 15:09:32 +0200

    Logiken från hooks
    Tog bort all logiken med hooks nu i app.js och main

--- src/App.js
+++ src/App.js
@@ -9,19 +9,6 @@ function App() {
   const [date, setDate] = useState({
     date1 : '2020-05-28'
   })
-  
-  const [mainPageState, setMainPageState] = useState(1)
-
- 
-  const homeButtonClicked = () =>{
-    setMainPageState(1);
-  }
-  const portfolioButtonClicked = () =>{
-    setMainPageState(2);
-  }
-  const settingsButtonClicked = () =>{
-    setMainPageState(3);
-  }
 
   const dummyProfile = PersonalInfo;
 
