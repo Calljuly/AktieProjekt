afd4e0c468052658dfb68df10cf9cf4aa513d515
Lime Fortytwo <melberik@gmail.com>
2020-06-16 16:36:26 +0200

    Now settingsPreference also grabs it's information from the database

--- src/Components/Pages/SettingsPage/PreferenceSettings/PreferenceSettings.js
+++ src/Components/Pages/SettingsPage/PreferenceSettings/PreferenceSettings.js
@@ -1,67 +1,80 @@
-import React from 'react';
-import PersonalInfo from '../../../../Data/PersonalInformation.json'
+import React, {useState, useEffect} from 'react';
 import PreferenceItem from './PreferenceItem'
 import {StyledForm, StyledTable, GrayP, Link, PreferenceContainer} from '../Styles'
 
-let suggestedIndustries = ["Byggsektorn", "Medtech", "Textech", "Spel", "Fintech", "Sovtech", "Läppglans"];
-let preferredIndustries = PersonalInfo.preferences;
 
-const editPreferences = (e, name) =>{
-    if (e.target.checked === false){
 
-        let index = preferredIndustries.indexOf(name);
-        preferredIndustries.splice(index, 1);
-    }
-    else{
-        preferredIndustries.push(name);
-    }
+const PreferencesSettings = ({username}) => {
     
-}
+    let suggestedIndustries = ["Byggsektorn", "Medtech", "Textech", "Spel", "Fintech", "Sovtech", "Läppglans"];
+    const [preferredIndustries, setPreferredIndustries] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-const writeOutAllSuggestions = () =>{
-    let result = [];
+    useEffect(() => {
+        fetch(`http://localhost:4001/users/${username}`)
+        .then(response => response.json())
+        .then(data => {
+            // console.log(JSON.parse(data.PersonalInformation).preferences);
+          setPreferredIndustries(JSON.parse(data.PersonalInformation).preferences);
+          setLoading(false);
+        });
+        }, []);
+
+    const editPreferences = (e, name) =>{
+        if (e.target.checked === false){
     
-    for (let i = 0; i < suggestedIndustries.length; i += 2) {
-        if ((i + 1) < suggestedIndustries.length){
-            result.push(
-                <tr key = {i}>
-                    <PreferenceItem 
-                        name = {suggestedIndustries[i]} 
-                        check = {preferredIndustries.includes(suggestedIndustries[i])}
-                        handleChange = {editPreferences}/>
-                    <PreferenceItem 
-                        name = {suggestedIndustries[i + 1]} 
-                        check = {preferredIndustries.includes(suggestedIndustries[i + 1])}
-                        handleChange = {editPreferences}
-                        />
-                </tr>
-            );
+            let index = preferredIndustries.indexOf(name);
+            setPreferredIndustries(preferredIndustries.splice(index, 1));
         }
-        else if (i === (suggestedIndustries.length - 1)){
-            result.push(
-                <tr key = {i}>
-                    <PreferenceItem 
-                        handleChange = {editPreferences}
-                        name = {suggestedIndustries[i]} 
-                        check = {preferredIndustries.includes(suggestedIndustries[i])}/>
-                </tr>
-            );
+        else{
+            preferredIndustries.push(name);
         }
         
     }
+    
+    const writeOutAllSuggestions = () =>{
+        let result = [];
+        
+        for (let i = 0; i < suggestedIndustries.length; i += 2) {
+            if ((i + 1) < suggestedIndustries.length){
+                console.log("checkie " +  preferredIndustries.includes(suggestedIndustries[i]));
+                result.push(
+                    <tr key = {i}>
+                        <PreferenceItem 
+                            name = {suggestedIndustries[i]} 
+                            check = {preferredIndustries.includes(suggestedIndustries[i])}
+                            handleChange = {editPreferences}/>
+                        <PreferenceItem 
+                            name = {suggestedIndustries[i + 1]} 
+                            check = {preferredIndustries.includes(suggestedIndustries[i + 1])}
+                            handleChange = {editPreferences}
+                            />
+                    </tr>
+                );
+            }
+            else if (i === (suggestedIndustries.length - 1)){
+                result.push(
+                    <tr key = {i}>
+                        <PreferenceItem 
+                            handleChange = {editPreferences}
+                            name = {suggestedIndustries[i]} 
+                            check = {preferredIndustries.includes(suggestedIndustries[i])}/>
+                    </tr>
+                );
+            }
+            
+        }
+    
+        return result;
+    }
 
-    return result;
-}
-
-const PreferencesSettings = ({username}) => {
-    console.log("Hihi" + username);
     return (
         
         <PreferenceContainer>
             <GrayP>Mina prefererade industrier att investera inom:</GrayP>
             <StyledForm>
                 <StyledTable>
-                    {writeOutAllSuggestions()}
+                    {!loading && writeOutAllSuggestions()}
                 </StyledTable>
             </StyledForm>
             <div style = {{maxWidth: '50%', fontSize: '14px'}}>
