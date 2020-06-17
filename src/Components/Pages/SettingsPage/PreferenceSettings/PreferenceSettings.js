5798796f30ed03ec25e799220d6e11e83977e5d4
Lime Fortytwo <melberik@gmail.com>
2020-06-17 22:07:51 +0200

    Sending objects with fetch and updating database with it now works

--- src/Components/Pages/SettingsPage/PreferenceSettings/PreferenceSettings.js
+++ src/Components/Pages/SettingsPage/PreferenceSettings/PreferenceSettings.js
@@ -5,47 +5,70 @@ import {StyledForm, StyledTable, GrayP, Link, PreferenceContainer} from '../Styl
 
 
 const PreferencesSettings = ({username}) => {
-    
+
     let suggestedIndustries = ["Byggsektorn", "Medtech", "Textech", "Spel", "Fintech", "Sovtech", "Läppglans"];
     const [preferredIndustries, setPreferredIndustries] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [personalInformation, setPersonalInformation] = useState({});
 
     useEffect(() => {
         fetch(`http://localhost:4001/users/${username}`)
         .then(response => response.json())
         .then(data => {
-            // console.log(JSON.parse(data.PersonalInformation).preferences);
+          setPersonalInformation(JSON.parse(data.PersonalInformation));
           setPreferredIndustries(JSON.parse(data.PersonalInformation).preferences);
           setLoading(false);
         });
+
+
+
         }, []);
+
+    // const updatePreferencesInDatabase = () =>{
+
+    // }
 
     const editPreferences = (e, name) =>{
         if (e.target.checked === false){
-    
+
             let index = preferredIndustries.indexOf(name);
+
+            // Needs a chache because splice returns the deleted items
             setPreferredIndustries(preferredIndustries.splice(index, 1));
         }
         else{
-            preferredIndustries.push(name);
+            let currentPreferredIndustries = preferredIndustries;
+            currentPreferredIndustries.push(name);
+            setPreferredIndustries(currentPreferredIndustries);
+
+            let currentPersonalInformation = personalInformation;
+            currentPersonalInformation.preferences = preferredIndustries;
+            setPersonalInformation(currentPersonalInformation);
+
+            fetch('http://localhost:4001/user/post', {
+            body: JSON.stringify({personalInformation: personalInformation}),
+            headers: {
+              "Content-Type": "application/json",
+            },
+            method: 'PATCH'
+          })
         }
-        
+
     }
-    
+
     const writeOutAllSuggestions = () =>{
         let result = [];
-        
+
         for (let i = 0; i < suggestedIndustries.length; i += 2) {
             if ((i + 1) < suggestedIndustries.length){
-                console.log("checkie " +  preferredIndustries.includes(suggestedIndustries[i]));
                 result.push(
                     <tr key = {i}>
-                        <PreferenceItem 
-                            name = {suggestedIndustries[i]} 
+                        <PreferenceItem
+                            name = {suggestedIndustries[i]}
                             check = {preferredIndustries.includes(suggestedIndustries[i])}
                             handleChange = {editPreferences}/>
-                        <PreferenceItem 
-                            name = {suggestedIndustries[i + 1]} 
+                        <PreferenceItem
+                            name = {suggestedIndustries[i + 1]}
                             check = {preferredIndustries.includes(suggestedIndustries[i + 1])}
                             handleChange = {editPreferences}
                             />
@@ -55,21 +78,21 @@ const PreferencesSettings = ({username}) => {
             else if (i === (suggestedIndustries.length - 1)){
                 result.push(
                     <tr key = {i}>
-                        <PreferenceItem 
+                        <PreferenceItem
                             handleChange = {editPreferences}
-                            name = {suggestedIndustries[i]} 
+                            name = {suggestedIndustries[i]}
                             check = {preferredIndustries.includes(suggestedIndustries[i])}/>
                     </tr>
                 );
             }
-            
+
         }
-    
+
         return result;
     }
 
     return (
-        
+
         <PreferenceContainer>
             <GrayP>Mina prefererade industrier att investera inom:</GrayP>
             <StyledForm>
--- src/Util/API/server.js
+++ src/Util/API/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const db = require('./db');
 const app = express();
+const md5 = require('md5');
 
 const PORT = process.env.PORT || 4001;
 
@@ -42,5 +43,33 @@ app.get('/users/:username/:password', (req, res, next) => {
     }
   })
 });
+
+app.patch("/user/post", (req, res, next) => {
+  
+  var data = {
+      personalInformation: req.body.personalInformation
+  };
+
+  // let stringy = JSON.stringify(data.personalInformation);
+  // let parsed = JSON.parse(stringy);
+
+  // console.log(parsed.fName);
+
+  db.run(
+      `UPDATE Users set PersonalInformation = (?) WHERE UserName = 'LiNi'`,
+      [JSON.stringify(data.personalInformation)],
+      
+      function (err, result) {
+          if (err){
+              res.status(400).json({"error": res.message})
+              return;
+          }
+          res.json({
+              message: "success",
+              data: data,
+              changes: this.changes
+          })
+  });
+})
 
 app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
