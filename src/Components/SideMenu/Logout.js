da2eecb6f95ff5d5225f1236c1b6f131c950f913
Lydia Lind <lydia.nilsson@gmail.com>
2020-06-18 15:01:46 +0200

    Fixed the log out functionality

--- src/Components/SideMenu/Logout.js
+++ src/Components/SideMenu/Logout.js
@@ -3,15 +3,20 @@ import LogOutPic from '../../images/logout.svg'
 import {LogOutStyle, OptionsImgStyle} from './Styles'
 import {useHistory} from 'react-router-dom'
 
-const LogOut = () => {
+const LogOut = ({setUserName}) => {
 
     const history = useHistory();
+    
+    const logOut = () => {
+        history.push('/')
+        setUserName();
+        sessionStorage.removeItem('username');
+    }
+
     return (
         <LogOutStyle >
         <OptionsImgStyle src={LogOutPic} alt="Log out "/>
-            <p onClick={()=>
-                history.push('/')
-            }>Logga ut</p>
+            <p onClick={logOut}>Logga ut</p>
         </LogOutStyle>
         )
     }
