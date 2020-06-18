77ee240bd269b2c2cd8b2cb808ec2e1b87a4e40f
Lydia Lind <lydia.nilsson@gmail.com>
2020-06-18 11:52:34 +0200

    Rearranged the styled components of ProfilePicture component to seperate file
    also updated syntax from ES5 to ES6

--- src/Components/Pages/StartPage/Profile/Profile.js
+++ src/Components/Pages/StartPage/Profile/Profile.js
@@ -24,7 +24,7 @@ const Profile = ({username}) => {
         return(
             <>
                 <ImageSummaryContainer>
-                    <ProfilePicture pictureName = {'testprofile'} isHomeScreen={true}/>
+                    <ProfilePicture isHomeScreen={true}/>
                     <HomeMyProfileSummary person = {personalInformation}/>
                 </ImageSummaryContainer>
                 <PreferredBusinesses person = {personalInformation}/>
