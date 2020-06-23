7251b24efc4e3aace0868315d18d22e2b5edaf86
Lydia Lind <lydia.nilsson@gmail.com>
2020-06-23 09:46:49 +0200

    Implement hashing of passwords with MD5

--- src/Util/API/server.js
+++ src/Util/API/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const  md5 = require('md5');
 
 const db = require('./db');
 const app = express();
@@ -17,7 +18,6 @@ app.get('/users/:username', (req, res, next) => {
       res.sendStatus(500);
     } else {
       if(row){
-        console.log(row);
         res.send(row);
       }
       else{
@@ -28,7 +28,7 @@ app.get('/users/:username', (req, res, next) => {
 });
 
 app.get('/users/:username/:password', (req, res, next) => {
-  db.get(`SELECT UserName FROM Users WHERE UserName = "${req.params.username}" AND Password = "${req.params.password}"`,(err, row) => {
+  db.get(`SELECT UserName FROM Users WHERE UserName = "${req.params.username}" AND Password = "${md5(req.params.password)}"`,(err, row) => {
     if (err) {
       res.sendStatus(500);
     } else {
